3cc24a8887b5cc8c7dbafcf477b687b4f3db1233
okada8 <1141912008@qq.com>
2019-05-12 19:22:14 +0800

    商品页面静态化

--- MeiDuo/meiduo_mall/celery_tasks/main.py
+++ MeiDuo/meiduo_mall/celery_tasks/main.py
@@ -13,5 +13,5 @@ celery_app = Celery('meiduo')
 celery_app.config_from_object('celery_tasks.config')
 
 # 自动发现任务
-celery_app.autodiscover_tasks(['celery_tasks.sms','celery_tasks.email'])
+celery_app.autodiscover_tasks(['celery_tasks.sms','celery_tasks.email','celery_tasks.html'])
 
